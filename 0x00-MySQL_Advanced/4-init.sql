b3ca6a32bd9733c3e8d412b2542efdea8bfa73e3
Kagisokgomo <kagisomalobela29@gmail.com>
2024-10-21 09:19:52 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-init.sql
+++ 0x00-MySQL_Advanced/4-init.sql
@@ -1,4 +1,4 @@
--- Initial
+-- Initial setup
 DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS orders;
 
@@ -12,4 +12,29 @@ CREATE TABLE IF NOT EXISTS orders (
     number int NOT NULL
 );
 
+-- Inserting sample data into items table
 INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+
+-- Creating the trigger
+DELIMITER $$
+
+CREATE TRIGGER update_item_quantity
+BEFORE INSERT ON orders
+FOR EACH ROW
+BEGIN
+    -- Update the quantity of the item in the items table by decreasing it
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END $$
+
+DELIMITER ;
+
+-- Sample orders
+INSERT INTO orders (item_name, number) VALUES ('apple', 1);
+INSERT INTO orders (item_name, number) VALUES ('apple', 3);
+INSERT INTO orders (item_name, number) VALUES ('pear', 2);
+
+-- Checking the result
+SELECT * FROM items;
+SELECT * FROM orders;
